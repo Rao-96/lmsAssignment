872df6f5084f6bdbb0667a6dde3a71ac16fcde9b
Ramith <53828605+Rao-96@users.noreply.github.com>
2022-06-11 11:08:53 +0530

    Initial commit

---
+++ src/main/java/com/lmsAssignment/lmsAssignment/LmsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lmsAssignment.lmsAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmsAssignment/lmsAssignment/LmsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsAssignment.lmsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
